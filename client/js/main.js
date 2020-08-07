47bc653a8107cbf6ad2dfa0cddc0b8e420bae75a
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-08-07 10:40:08 +0100

    Mount Svelte components

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import Test from "../../components/audio.svelte";
+
+const audioEls = document.querySelectorAll(".audio");
+
+console.log("hello world!");
+
+for (const el of audioEls) {
+  const player = el.querySelector("audio");
+  new Test({
+    target: el,
+    props: {
+      player,
+    },
+  });
+}
--- tools/rollup.config.js
+++ tools/rollup.config.js
@@ -1,44 +1,38 @@
+import resolve from "@rollup/plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
+import postcss from "rollup-plugin-postcss";
+import commonjs from "rollup-plugin-commonjs";
+
+const production = !process.env.ROLLUP_WATCH;
+
+/**
+ * @param   {string}  [path]  slash-prefixed path relative to _site/lib
+ * @return  {string}
+ */
+function dest(path) {
+  return "_site/lib" + (path ? path : "");
+}
 
 export default {
-  input: "src/js/main.js",
+  input: "client/js/main.js",
   output: {
-    dir: "_site/lib",
+    dev: !production,
+    dir: dest(),
     format: "es",
   },
   plugins: [
+    postcss({}),
+    resolve({
+      browser: true,
+      dedupe: (importee) =>
+        importee === "svelte" || importee.startsWith("svelte/"),
+    }),
+    commonjs(),
+
     svelte({
-      // Optionally, preprocess components with svelte.preprocess:
-      // https://svelte.dev/docs#svelte_preprocess
-      // preprocess: {
-      //   style: ({ content }) => {
-      //     return transformStyles(content);
-      //   },
-      // },
-
-      // Emit CSS as "files" for other plugins to process
       emitCss: true,
-
-      // Extract CSS into a separate file (recommended).
-      // See note below
       css: function (css) {
-        console.log(css.code); // the concatenated CSS
-        console.log(css.map); // a sourcemap
-
-        // creates `main.css` and `main.css.map` — pass `false`
-        // as the second argument if you don't want the sourcemap
-        css.write("_site/lib/main.css", true);
-      },
-
-      // Warnings are normally passed straight to Rollup. You can
-      // optionally handle them here, for example to squelch
-      // warnings with a particular code
-      onwarn: (warning, handler) => {
-        // e.g. don't warn on <marquee> elements, cos they're cool
-        if (warning.code === "a11y-distracting-elements") return;
-
-        // let Rollup handle all other warnings normally
-        handler(warning);
+        css.write(dest("/main.css"), true);
       },
     }),
   ],
